1756e5a2e9d015de443cbd8b74890374ed083fdb
Jesus Miranda <jesusmiranda0304@gmail.com>
2018-08-05 15:13:19 -0500

    Creada la clase MediaViewModel
    Para q serieViewModel y MovieViewModel puedan herdar de ella

---
+++ VideoOnDemand.Web/Models/MediaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VideoOnDemand.Entities;
+
+namespace VideoOnDemand.Web.Models
+{
+    public class MediaViewModel
+    {
+        public int? MediaId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int? DuracionMin { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public DateTime? FechaLanzamiento { get; set; }
+
+        public ICollection<Genero> Generos { get; set; }
+        public ICollection<Persona> Actores { get; set; }
+        public ICollection<Opinion> Opiniones { get; set; }
+        public EEstatusMedia? EstadosMedia { get; set; }
+    }
+}
--- VideoOnDemand.Web/Models/MovieViewModel.cs
+++ VideoOnDemand.Web/Models/MovieViewModel.cs
@@ -5,13 +5,8 @@ using System.Web;
 
 namespace VideoOnDemand.Web.Models
 {
-    public class MovieViewModel
+    public class MovieViewModel : MediaViewModel
     {
-        public int? MediaId { get; set; }
-        public string Nombre { get; set; }
-        public string Descripcion { get; set; }
-        public int? DuracionMin { get; set; }
-        public DateTime? FechaRegistro { get; set; }
-        public DateTime? FechaLanzamiento { get; set; }
+
     }
 }
