47daffd6405d23b644053ae339a29a3e1cb098ea
Jesus Miranda <jesusmiranda0304@gmail.com>
2018-08-05 15:03:57 -0500

    Agregado
    La vista de Genero y La vista de catalogo de peliculas, aun me falta terminarla pero ya busca y muestra las peliculas agregadas  puestas como visibles.

--- VideoOnDemand.Web/Helpers/MapHelper.cs
+++ VideoOnDemand.Web/Helpers/MapHelper.cs
@@ -21,7 +21,7 @@ namespace VideoOnDemand.Web.Helpers
                 x.CreateMap<Episodio, EpisodioViewModel>().ReverseMap();
                 x.CreateMap<Episodio, NuevoEpisodioViewModel>().ReverseMap();
                 x.CreateMap<Episodio, ModificadoEpisodioViewModel>().ReverseMap();
-
+                x.CreateMap<Genero, GeneroViewModel>().ReverseMap();
             });
             mapper = config.CreateMapper();
         }
