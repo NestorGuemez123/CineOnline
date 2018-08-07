f2facd5d0b5ff43ea66bd53016e6ed983ea1b7f7
Jesus Miranda <jesusmiranda0304@gmail.com>
2018-08-06 19:40:48 -0500

    Opinion
    A medias.

--- VideoOnDemand.Web/Controllers/OpinionController.cs
+++ VideoOnDemand.Web/Controllers/OpinionController.cs
@@ -3,32 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoOnDemand.Repositories;
+using VideoOnDemand.Web.Helpers;
 using VideoOnDemand.Web.Models;
 
 namespace VideoOnDemand.Web.Controllers
 {
-    public class OpinionController : Controller
+    public class OpinionController : BaseController
     {
         // GET: Opinion
         public ActionResult Index()
-        {
-            return View();
+        {   
+            OpinionRepository repository = new OpinionRepository(context);
+            var lst = repository.GetAll();
+            var models = MapHelper.Map<IEnumerable<OpinionViewModel>>(lst);
+
+            return View(models);
         }
 
-        public ActionResult Create()
+        public ActionResult Create(int? id)
         {
+            OpinionRepository repository = new OpinionRepository(context);
+            UsuarioRepository repositoryUsuario = new UsuarioRepository(context);
+            try{
+                var us = Session["UserId"] as string;
+
+                var media = repository.Query(t => t.MediaId == id).FirstOrDefault();
+                var model = MapHelper.Map<OpinionViewModel>(media);
+
+                var usuarios = repositoryUsuario.Query(u => u.IdentityId.Equals(us)).FirstOrDefault();
+
+                return View(model);
+            }
+            catch(Exception ex)
+            {
+
+            }
             return View();
         }
 
         // POST: Genero/Create
         [HttpPost]
-        public ActionResult Create(OpinionViewModel model)
+        public ActionResult Create(int? id, OpinionViewModel model)
         {
             try
             {
-                                
-
-                return RedirectToAction("Index");
+                OpinionRepository repository = new OpinionRepository(context);
+                var persona = repository.Query(t => t.MediaId == id).First();
+                var models = MapHelper.Map<OpinionViewModel>(persona);
+                return View(models);                
             }
             catch (Exception ex)
             {
