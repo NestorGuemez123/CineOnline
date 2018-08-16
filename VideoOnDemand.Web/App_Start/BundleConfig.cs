daa8602ebda7e7107dd3f9c4cd7711e2460d722e
NestorGuemez123 <nestorguemezche@hotmail.com>
2018-08-15 21:50:45 -0500

    cambios para cambiar de login y seguir trabajando

--- VideoOnDemand.Web/App_Start/BundleConfig.cs
+++ VideoOnDemand.Web/App_Start/BundleConfig.cs
@@ -10,7 +10,11 @@ namespace VideoOnDemand.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/star-rater").Include(
+                       "~/Scripts/jquery.star-rating-svg.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
